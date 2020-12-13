dc0759224e737abbce56db25a1f7505de9cd805f
Ilya <ilay.ik@gmail.com>
2020-12-13 14:13:48 +0300

    Auth with custom pageLogni

--- src/main/java/ru/ilay/springsecuriryrest/config/SecurityConfig.java
+++ src/main/java/ru/ilay/springsecuriryrest/config/SecurityConfig.java
@@ -11,7 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.ilay.springsecuriryrest.model.Role;
+
+import java.lang.reflect.Method;
 
 
 @Configuration
@@ -27,7 +30,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/auth/login").permitAll()
+                .defaultSuccessUrl("/auth/success")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/auth/login");
     }
 
     @Bean
